d5dca717cdbbf19d80323c1231e55773ccdab70b
Maciej <hoffix0123@gmail.com>
2021-12-15 17:42:31 +0100

    feat: Data display on Board Content

--- src/components/BoardListElement.tsx
+++ src/components/BoardListElement.tsx
@@ -1,8 +1,17 @@
 import React, { FC } from "react";
 import styled from "styled-components";
 const StyledBoardListElement = styled.div``;
-const BoardListElement: FC = () => {
-  return <StyledBoardListElement></StyledBoardListElement>;
+
+interface Props {
+  todo: {
+    todoTitle: string;
+    todoStatus: string;
+    todoTimeLeft: number;
+  };
+}
+
+const BoardListElement: FC<Props> = ({ todo }) => {
+  return <StyledBoardListElement>{todo.todoTitle}</StyledBoardListElement>;
 };
 
 export default BoardListElement;
